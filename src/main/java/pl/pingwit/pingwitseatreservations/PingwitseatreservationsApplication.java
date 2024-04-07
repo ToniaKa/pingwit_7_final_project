c277dd4513e28e8aaf16075649ab14ca76e61c89
Tonya Kazlovskaya <toshichka.91@gmail.com>
2024-04-07 14:02:24 +0300

    initial commit

---
+++ src/main/java/pl/pingwit/pingwitseatreservations/PingwitseatreservationsApplication.java
@@ -0,0 +1,13 @@
+package pl.pingwit.pingwitseatreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingwitseatreservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingwitseatreservationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pingwit/pingwitseatreservations/PingwitseatreservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pingwit.pingwitseatreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingwitseatreservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
