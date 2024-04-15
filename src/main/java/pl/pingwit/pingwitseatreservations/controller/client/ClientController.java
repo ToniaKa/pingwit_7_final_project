1e3c5d2cd6a580a3b4e78aa9805a11e6ae331741
Pavel Radkevich <pradkevich@eisgroup.com>
2024-04-15 18:34:04 +0300

    Answered the question regarding ClientController

--- src/main/java/pl/pingwit/pingwitseatreservations/controller/client/ClientController.java
+++ src/main/java/pl/pingwit/pingwitseatreservations/controller/client/ClientController.java
@@ -7,6 +7,7 @@ import pl.pingwit.pingwitseatreservations.service.booking.BookingService;
 import pl.pingwit.pingwitseatreservations.service.client.ClientService;
 
 import java.util.List;
+
 @Tag(name = "Client API")
 @RestController
 @RequestMapping("/client")
@@ -18,14 +19,33 @@ public class ClientController {
         this.clientService = clientService;
         this.bookingService = bookingService;
     }
+
     @GetMapping
-    public List<ClientDto> listClients(){return clientService.listClients();}
+    public List<ClientDto> listClients() {
+        return clientService.listClients();
+    }
+
     @GetMapping("/{id}")
-    public ClientFullDto getClient(@PathVariable(name = "id") Integer id){return clientService.getClient(id);}
+    public ClientFullDto getClient(@PathVariable(name = "id") Integer id) {
+        return clientService.getClient(id);
+    }
+
     @PostMapping
-    public Integer createClient(@RequestBody CreateClientDto clientDto){return clientService.createClient(clientDto);}
+    public Integer createClient(@RequestBody CreateClientDto clientDto) {
+        return clientService.createClient(clientDto);
+    }
+
     @PutMapping("/{id}")
-    public void updateClient(@RequestBody UpdateClientInputDto inputDto,@PathVariable(name = "id") Integer id){clientService.updateClient(id,inputDto);}
-    @GetMapping("/{id}/booking")
-    public BookingDto getBooking(@PathVariable(name = "id") Integer id){return  bookingService.getBooking(id);}
+    public void updateClient(@RequestBody UpdateClientInputDto inputDto, @PathVariable(name = "id") Integer id) {
+        clientService.updateClient(id, inputDto);
+    }
+
+    @GetMapping("/{clientId}/booking") // здесь работаем с айдишкой клиента
+    public BookingDto getBooking(@PathVariable(name = "clientId") Integer clientId) {
+        // этот метод должен возвращать List<BookingDto> и называться как-то
+        // getClientBookings
+        // в bookingService нужно создать метод, который вернет список пронирований по айди клиента
+        // это можно сделать, написав в репозитории метод findAllByClientId
+        return bookingService.getBooking(id);
+    }
 }
